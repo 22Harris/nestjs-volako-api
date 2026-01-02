c4a4e277f36a8c911c7a299459351db2042a57b6
Harris Martial <harrismartial02@gmail.com>
2026-01-02 12:17:04 +0300

    add operation on database

--- prisma/migrations/20260102091218_init_wallet_db/migration.sql
+++ prisma/migrations/20260102091218_init_wallet_db/migration.sql
@@ -1,4 +1,14 @@
 -- CreateTable
+CREATE TABLE "Operation" (
+    "id" SERIAL NOT NULL,
+    "type" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "label" TEXT NOT NULL,
+
+    CONSTRAINT "Operation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Account" (
     "id" SERIAL NOT NULL,
     "code" TEXT NOT NULL,
@@ -13,6 +23,7 @@ CREATE TABLE "JournalEntry" (
     "id" SERIAL NOT NULL,
     "date" TIMESTAMP(3) NOT NULL,
     "label" TEXT NOT NULL,
+    "operationId" INTEGER,
 
     CONSTRAINT "JournalEntry_pkey" PRIMARY KEY ("id")
 );
@@ -27,6 +38,9 @@ CREATE TABLE "JournalLine" (
 
     CONSTRAINT "JournalLine_pkey" PRIMARY KEY ("id")
 );
+
+-- AddForeignKey
+ALTER TABLE "JournalEntry" ADD CONSTRAINT "JournalEntry_operationId_fkey" FOREIGN KEY ("operationId") REFERENCES "Operation"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "JournalLine" ADD CONSTRAINT "JournalLine_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
